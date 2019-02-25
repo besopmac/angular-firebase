0492c6e4b9f5d5933ca57d110853ea70319ae5c5
Bruno Campos <brunosopmac@gmail.com>
2019-02-25 19:01:33 -0300

    HttpClient tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeUpdateComponent } from './employee-update/employee-update.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+  { path: 'create-employee', component: EmployeeCreateComponent },
+  { path: 'employee-details/:id', component: EmployeeDetailsComponent },
+  { path: 'update-employee', component: EmployeeUpdateComponent },
+  { path: 'employees-list', component: EmployeesListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+// HttpClient module for RESTful API
+import { HttpClientModule } from '@angular/common/http';
+
+// Routing module for router service
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+// Firebase
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 
+// Forms module
+import { FormsModule } from '@angular/forms';
+
+// Components
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeUpdateComponent } from './employee-update/employee-update.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeDetailsComponent,
+    EmployeeUpdateComponent,
+    EmployeesListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     AngularFireModule.initializeApp(environment.firebaseConfig, 'my-project-demo')
   ],
   providers: [],
